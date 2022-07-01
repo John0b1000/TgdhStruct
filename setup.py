796b83e956214c2edf7ee04a5aee6d99d2a7c0cb
John Nori <johnlnori8@gmail.com>
2022-07-01 13:26:34 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+from src import binary_tree
+
+setup(
+    name='tgdhstruct',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/John0b1000/tgdhstruct',
+    author='John Nori',
+    author_email='johnlnori8@gmail.com',
+    description='tgdh structure library'
+)
